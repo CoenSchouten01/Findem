818bc6272c59d56bd4e8155cc7d4023525153d39
Coen Schouten <coenschouten2001@gmail.com>
2020-09-23 13:25:40 +0200

    Wat geklooi met addItem

--- app/src/main/java/com/example/findem/AddItem.java
+++ app/src/main/java/com/example/findem/AddItem.java
@@ -3,6 +3,8 @@ package com.example.findem;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddItem extends AppCompatActivity {
 
@@ -13,5 +15,12 @@ public class AddItem extends AppCompatActivity {
 
         // Get the intent that started this activity
         Intent intent = getIntent();
+    }
+
+    public void add_new_item(View view){
+        EditText define_itemName_textField = (EditText)  findViewById(R.id.define_itemName_textField);
+
+        String name = define_itemName_textField.getText().toString();
+        Item(name, image)
     }
 }
---
+++ app/src/main/java/com/example/findem/Item.java
@@ -0,0 +1,11 @@
+package com.example.findem;
+
+public class Item {
+    String name; //A string that contains the name of the item
+    String image; //A string that contains the path to the image of the item
+    //constructor
+    Item(String name, String image){
+        this.name = name;
+        this.image = image;
+    }
+}
