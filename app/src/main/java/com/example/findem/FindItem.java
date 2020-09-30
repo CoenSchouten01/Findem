4330af97446c07b05f7ba28d57827303352f2d44
Yabbanootje <kelvintoonen2001@outlook.com>
2020-09-30 16:18:49 +0200

    Changed Toast to be more informing

--- app/src/main/java/com/example/findem/FindItem.java
+++ app/src/main/java/com/example/findem/FindItem.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 
 import java.io.BufferedReader;
