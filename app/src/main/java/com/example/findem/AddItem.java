c0a6978329268591eaacf6131020d1a2b395a1b5
Coen Schouten <coenschouten2001@gmail.com>
2020-09-30 14:03:51 +0200

    wat leuke changes

--- app/src/main/java/com/example/findem/AddItem.java
+++ app/src/main/java/com/example/findem/AddItem.java
@@ -5,6 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class AddItem extends AppCompatActivity {
@@ -25,11 +33,56 @@ public class AddItem extends AppCompatActivity {
     public void add_new_item(View view){
         //
         EditText define_itemName_textField = findViewById(R.id.define_itemName_textField);
-
-        //Assign values to the strings
+        EditText define_itemImage_textField = findViewById(R.id.define_itemImage_textField);
+//        //Assign values to the strings
         String name = define_itemName_textField.getText().toString();
-        //construct an Item based on the name and image
-        Item new_item = new Item(name);
-        define_itemName_textField.setText(new_item.name + " " + "toegevoegd");
+//        //construct an Item based on the name and image
+//        Item new_item = new Item(name);
+//        define_itemName_textField.setText(new_item.name + " " + "toegevoegd");
+        FileOutputStream fos = null;
+
+        try {
+            fos = openFileOutput(MainActivity.FILE_NAME, MODE_PRIVATE);
+            fos.write(name.getBytes());
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/" +
+                            MainActivity.FILE_NAME, Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(MainActivity.FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("\n");
+            }
+            define_itemImage_textField.setText(sb.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/findem/MainActivity.java
+++ app/src/main/java/com/example/findem/MainActivity.java
@@ -6,19 +6,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static File items;
+    public static final String FILE_NAME = "items.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        items = new File("files/items.txt");
     }
 
     //This function navigates the user to the AddItem page when this button on the main page is clicked
